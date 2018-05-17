e952fd700a2a390856d3af7f620a7c0d99cd6f81
patrick <pitrackster@gmail.com>
2018-05-17 12:03:43 +0200

    real user entity update

--- application/src/Entity/User.php
+++ application/src/Entity/User.php
@@ -1,26 +1,35 @@
 <?php
 
-
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 
 /**
- * @ORM\Table(name="app_users")
+ * @ORM\Table(name="app_user")
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
 class User implements AdvancedUserInterface, \Serializable
 {
     /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=25, unique=true)
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=25)
      */
     private $username;
 
@@ -30,96 +39,209 @@ class User implements AdvancedUserInterface, \Serializable
     private $password;
 
     /**
-     * @var string
+     * @ORM\Column(type="string", length=255)
      */
     private $plainPassword;
 
     /**
-     * @ORM\Column(type="string", length=254, unique=true)
+     * @ORM\Column(type="string", length=255)
      */
     private $email;
 
     /**
-     * @ORM\Column(name="is_active", type="boolean")
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
-    private $isActive;
+    private $image;
 
-    /** @ORM\Column(type="json")
-     *
+    /**
+     * @ORM\Column(type="text", nullable=true)
      */
-    private $roles = [];
+    private $description;
 
-    public function __construct()
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $publicMail;
+
+    /**
+     * @ORM\Column(type="json_array")
+     */
+    private $roles;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $active;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
+
+    public function getId()
     {
-        $this->isActive = true;
+        return $this->id;
     }
 
-    public function getUsername()
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
     {
         return $this->username;
     }
 
-    public function setUsername(string $username)
+    public function setUsername(string $username): self
     {
         $this->username = $username;
+
+        return $this;
     }
 
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email)
-    {
-        $this->email = $email;
-    }
-
-    public function setPassword(string $password)
-    {
-        $this->password = $password;
-    }
-
-    public function getPassword()
+    public function getPassword(): ?string
     {
         return $this->password;
     }
 
-    public function getPlainPassword()
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getPlainPassword(): ?string
     {
         return $this->plainPassword;
     }
 
-    public function setPlainPassword(string $plainPassword)
+    public function setPlainPassword(string $plainPassword): self
     {
         $this->plainPassword = $plainPassword;
+
+        return $this;
     }
 
-
-    public function getRoles(): array
+    public function getEmail(): ?string
     {
-        //return array_unique(array_merge(['ROLE_USER'], $this->roles));
-        return array_unique($this->roles);
+        return $this->email;
     }
 
-    public function setRoles(array $roles)
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPublicMail(): ?bool
+    {
+        return $this->publicMail;
+    }
+
+    public function setPublicMail(bool $publicMail): self
+    {
+        $this->publicMail = $publicMail;
+
+        return $this;
+    }
+
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    public function setRoles($roles): self
     {
         $this->roles = $roles;
+
+        return $this;
     }
 
-    public function resetRoles()
+    public function getActive(): ?bool
     {
-        $this->roles = [];
+        return $this->active;
     }
 
-    // only to satisfy interface
-    public function getSalt()
+    public function setActive(bool $active): self
     {
-        return null;
+        $this->active = $active;
+
+        return $this;
     }
 
-    // only to satisfy interface
-    public function eraseCredentials()
+    public function getCreatedAt(): ?\DateTimeInterface
     {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
     }
 
     /** @see \Serializable::serialize() */
@@ -127,10 +249,15 @@ class User implements AdvancedUserInterface, \Serializable
     {
         return serialize(array(
           $this->id,
+          $this->firstname,
+          $this->lastname,
           $this->username,
           $this->password,
           $this->email,
+          $this->publicMail,
           $this->isActive,
+          $this->createdAt,
+          $this->updatedAt,
       ));
     }
 
@@ -140,10 +267,20 @@ class User implements AdvancedUserInterface, \Serializable
         list(
           $this->id,
           $this->username,
+          $this->firstname,
+          $this->lastname,
           $this->password,
           $this->email,
+          $this->publicMail,
           $this->isActive,
+          $this->createdAt,
+          $this->updatedAt,
       ) = unserialize($serialized, ['allowed_classes' => false]);
+    }
+
+    public function isEnabled()
+    {
+        return $this->isActive;
     }
 
     public function isAccountNonExpired()
@@ -161,8 +298,14 @@ class User implements AdvancedUserInterface, \Serializable
         return true;
     }
 
-    public function isEnabled()
+    // only to satisfy interface
+    public function getSalt()
     {
-        return $this->isActive;
+        return null;
+    }
+
+    // only to satisfy interface
+    public function eraseCredentials()
+    {
     }
 }
--- application/src/Form/UserType.php
+++ application/src/Form/UserType.php
@@ -6,18 +6,24 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
             ->add('email', EmailType::class)
             ->add('username', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('publicMail', CheckboxType::class)
             ->add('plainPassword', RepeatedType::class, array(
                 'type' => PasswordType::class,
                 'options' => array('attr' => array('class' => 'password-field')),
