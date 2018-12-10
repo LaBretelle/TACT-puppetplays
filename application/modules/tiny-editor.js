230e0447a6ac177385806fa1da4eacda6ddda098
pitrackster <pitrackster@gmail.com>
2018-12-10 15:10:14 +0100

    fix #67 fix #60

--- application/modules/tiny-editor.js
+++ application/modules/tiny-editor.js
@@ -5,16 +5,21 @@ const Tiny = require('tinymce')
 require('tinymce/themes/modern/theme')
 // Any plugins you want to use has to be imported
 require('tinymce/plugins/link')
+require('tinymce/plugins/paste')
 
 class TinyEditor {
   constructor(){
-    console.log('yope')
+    //console.log('yope')
   }
-  
+
   init() {
     Tiny.init({
       selector: 'textarea.tinymce-enabled',
-      plugins: ['link'],
+      plugins: ['link', 'paste'],
+      //paste_word_valid_elements: 'b,strong,i,em,h1,h2',
+      //paste_retain_style_properties: 'color',
+      //paste_enable_default_filters: false,
+      paste_as_text: true,
       menubar: false,
       statusbar: false,
       setup: (editor) => {
