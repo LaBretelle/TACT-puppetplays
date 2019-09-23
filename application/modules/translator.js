e9f1d912cf94a21d966db00f713999b23d125167
hatiers <sylvain.hatier@univ-grenoble.alpes.fr>
2019-09-23 14:29:30 +0200

    Ne plus charger les translations TEI

--- application/modules/translator.js
+++ application/modules/translator.js
@@ -1,8 +1,6 @@
 /* global require module */
 const Translator = require('bazinga-translator')
 
-/*
-
 // this version loads translations from messages domain (and only this domain)
 const url = Routing.generate('bazinga_jstranslation_js', {
   _format: 'json'
@@ -14,42 +12,6 @@ $.ajax({
 }).done((response) => {
   // translations from messages domain (and only this domain) are now loaded
   Translator.fromJSON(response)
-})
-*/
-
-
-let translations = {}
-
-// this will load ONLY default trans domain... and it can not be ocnfigured to load multiple domains at once
-const url = Routing.generate('bazinga_jstranslation_js', {
-  _format: 'json'
-})
-
-
-
-$.ajax({
-  method: 'GET',
-  url: url
-}).done((response) => {
-  // messages domain are now loaded
-  translations = response
-}).then(() => {
-  // quite awfull isn't it ?
-  const url = Routing.generate('bazinga_jstranslation_js', {
-    _format: 'json',
-    domain: 'tei'
-  })
-
-  $.ajax({
-    method: 'GET',
-    url: url
-  }).done((response) => {
-    // tei domaine are now loaded
-    translations.translations[Translator.locale].tei = response.translations[Translator.locale].tei
-  }).then(() => {
-    Translator.fromJSON(translations)
-  })
-
 })
 
 module.exports = Translator
