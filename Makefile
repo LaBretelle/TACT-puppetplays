286d7886455ec4ae92da67a165e8f1eae8e62a33
arnaudbey <arnaud.bey@univ-grenoble-alpes.fr>
2021-11-25 14:21:22 +0100

    fix #203 (+npm related stuff)

--- Makefile
+++ Makefile
@@ -6,5 +6,6 @@ init:
 	openssl genrsa 2048 > docker-files/certs/server.key
 	openssl req -new -x509 -nodes -sha256 -days 365 -key docker-files/certs/server.key -out docker-files/certs/server.crt
 	cp docker-files/certs/server.crt docker-files/certs/server-chain-file.crt
+	docker-compose build --no-cache
 	docker-compose up -d
 	docker-compose exec apache make init
--- application/config/bundles.php
+++ application/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     Bazinga\Bundle\JsTranslationBundle\BazingaJsTranslationBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
